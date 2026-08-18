cad1e384472e2c2487ade8184224a19b6279e5c0
Craig Yanitski <yanitski@ph1.uni-koeln.de>
2026-08-18 00:16:51 -0600

    ensure fetch request follows redirects
    * ensure base url is always a string

--- src/api/files.ts
+++ src/api/files.ts
@@ -1,12 +1,16 @@
 //import type { FtpFile } from '../types/file'
 
-const BASE_URL = import.meta.env.VITE_YANITSKIBOX_MEDIA_PORT
+const BASE_URL = import.meta.env.VITE_YANITSKIBOX_MEDIA_PORT || 'localhost'
 
 export async function listFiles(path = '/'): Promise<string> {
-  console.log(`${BASE_URL}/${path}`)
-  const res = await fetch(`${BASE_URL}/${path}`)
+  const mediaURL = new URL(path, `http://${BASE_URL}`);//`http://${BASE_URL}/${path}`;
+  console.log(mediaURL.href);
+  const res = await fetch(mediaURL, {
+    method: "GET",
+    redirect: "follow",
+  });
   if (!res.ok) {
-    throw new Error(`Failed to list files (${res.status})`)
+    throw new Error(`Failed to list files (${res.status})`);
   }
-  return res.text()
+  return res.text();
 }
